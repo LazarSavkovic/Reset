0260bf04b44f7591ad887c7f92473c6017b5f48a
Lazar Savkovic <savkoviclazar1@gmail.com>
2025-03-30 14:56:29 +0200

    add requirements 2

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 
+from flask import Flask, render_template, request, flash
 app = Flask(__name__)
+
+
+
 
 @app.route("/")
 def home():
@@ -12,4 +16,5 @@ def terapeut(name):
     return render_template("index.html", name=name)
 
 if __name__ == "__main__":
+    app.secret_key = "your_secret_key"
     app.run(debug=True)
